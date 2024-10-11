d795bfe6c7ccfe44c60b429d262964e307845927
avonggeorge <georgeavong@yahoo.com>
2024-10-11 03:25:02 +0100

    Added a new line

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Module for concatination.
-"""
+'''Module for concatination.
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings.
-    """
+    '''Concatenates two strings.
+    '''
     return str1 + str2
