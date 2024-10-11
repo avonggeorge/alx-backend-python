a918307d6efaa2e5ecc549bcd74efb79165a4e5e
avonggeorge <georgeavong@yahoo.com>
2024-10-11 03:12:11 +0100

    Modified and added new line

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""
-Task number 7
-"""
+'''Task 7's module.
+'''
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Returns a tuple with the string k and the square of v as a float."""
-    return (k, float(v ** 2))
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
