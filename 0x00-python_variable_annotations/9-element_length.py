96c930f3af6c2d9d06e656ede0095267e5c8c211
avonggeorge <georgeavong@yahoo.com>
2024-10-11 03:09:26 +0100

    Modified and added new line

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-"""Task number 9"""
-from typing import Iterable, Sequence, List, Tuple
+'''Task 9's module.
+'''
+from typing import Iterable, List, Sequence, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Return a list of tuples containing elements and their lengths."""
+    '''Computes the length of a list of sequences.
+    '''
     return [(i, len(i)) for i in lst]
